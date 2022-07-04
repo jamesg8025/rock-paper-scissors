8c1baab9dc18cf67537b5c6d0e5dbf4e2532d045
James Gonzalez <jamesg8025@gmail.com>
2022-07-03 22:53:35 -0500

    Start from scratch in seperate .js doc

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// Create array of possible choices - all lowercase
+const choices = ['rock', 'paper', 'scissors']
+
+// Function to loop through five rounds and decide game winner
+function game() {
+
+}
+
+// Plays one round and determines winner after
+function playRound() {
+
+}
+
+// Function to get entry from player
+function playerPlay () {
+
+}
+
+// Get random entry from computer
+function computerPlay() {
+
+}
