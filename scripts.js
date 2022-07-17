d5d6f81355aea4194b8fae344ca782b189806e7d
James Gonzalez <jamesg8025@gmail.com>
2022-07-16 23:50:47 -0500

    Add functions to create wins array and tally scores

--- scripts.js
+++ scripts.js
@@ -1,69 +1,79 @@
-// Create array of possible choices - all lowercase
-const choices = ['rock', 'paper', 'scissors']
-// // Create empty array to log winners
-// const winners = []
+// Create empty array to log winners
+const winners = []
 
 let playerScore = 0;
 let computerScore = 0;
 
-// Function to loop through five rounds and decide game winner
-function game() {
-    // for (let i = 0; i < 5; i++) {
-    //     playRound(i);
-    // }
+// reset game
+function resetGame() {
 
-    // Compare the two scores after five rounds and determine who won the game
-    if (playerScore < computerScore) {
-        console.log('Sorry! Computer wins the game!');
-    } else if (playerScore > computerScore){
-        console.log('Congrats! You win the game!');
-    } else {
-        console.log('Tie!');
-    }
 }
 
+// Play game until someone wins 5 times
+function startGame() {
+    // this will grab all button tags in html
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => 
+    button.addEventListener(('click'), () => {
+        if (button.id) {
+            playRound(button.id);
+        }
+    }))
+}
+
+// // Play game until someone wins 5 times
+// function game() {
+//     // Compare the two scores after five rounds and determine who won the game
+//     if (playerScore < computerScore) {
+//         console.log('Sorry! Computer wins the game!');
+//     } else if (playerScore > computerScore){
+//         console.log('Congrats! You win the game!');
+//     } else {
+//         console.log('Tie!');
+//     }
+// }
+
 // Plays one round and determines winner after
-function playRound() {
-    const playerSelection = playerPlay();
-    console.log('You chose:', playerSelection);
+function playRound(playerSelection) {
+    // first check if anyone has 5 wins or not
+    let wins = checkWins();
+    if (wins >= 5) {
+        return;
+    }
+
     const computerSelection = computerPlay();
     console.log('Computer chose:', computerSelection);
     const roundWinner = checkRoundWin(playerSelection, computerSelection);
     console.log(roundWinner);
-    // const winner = checkRoundWin(playerSelection, computerSelection);
-    // // This function pushes a new winner into the winners array
-    // winners.push(winner);
+    const winner = checkRoundWin(playerSelection, computerSelection);
+    // This function pushes a new winner into the winners array
+    winners.push(winner);
+    // This function updates scores
+    tallyWins();
+    // Function displays what each player chose
+    displayRound(playerSelection, computerSelection, winner);
 }
 
-// Function to get player's entry
-function playerPlay () {
-    let playerChoice = prompt('Choose: Rock, Paper, or Scissors');
-    // This is in case player presses 'Cancel'
-    while (playerChoice == null) {
-        playerChoice = prompt('Choose something! Rock, Paper, or Scissors');
-    }
-    playerChoice = playerChoice.toLowerCase();
-    let check = validatePlayerChoice(playerChoice);
-    // In case user entry is not in choices array
-    while (check == false) {
-        // Reassign playerChoice to whatever they type based on the prompt - this stops the infinite reprompting
-        playerChoice = prompt('Must choose between: Rock, Paper or Scissors!');
-        while (playerChoice == null) {
-            playerChoice = prompt('Choose something! Rock, Paper, or Scissors');
-        }
-        playerChoice = playerChoice.toLowerCase();
-        check = validatePlayerChoice(playerChoice);
-    }
-    return playerChoice;
+
+
+function tallyWins() {
+    const pWinCount = winners.filter((item) => item == 'Player').length;
+    const cWinCount = winners.filter((item) => item == 'Computer').length;
+    const draws = winners.filter((item) => item == 'Tie').length;
+    document.querySelector('.playerScore').textContent = `Score: ${pWinCount}`;
+    document.querySelector('.computerScore').textContent = `Score: ${cWinCount}`;
+    document.querySelector('.draws').textContent = `Score: ${draws}`;
 }
 
-// Validate user input is in choices array
-function validatePlayerChoice(choice) {
-    return choices.includes(choice);
+function checkWins() {
+    const pWinCount = winners.filter((item) => item == 'Player').length;
+    const cWinCount = winners.filter((item) => item == 'Computer').length;
+    return Math.max(pWinCount, cWinCount); // this function returns the highest of two parameters
 }
 
 // Get random entry from computer
 function computerPlay() {
+    // TODO: update DOM with computer selection
     // Randomly select from choices array
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
@@ -110,6 +120,11 @@ function checkRoundWin(choiceP, choiceC) {
     }
 }
 
+function setWins() {
+    const pWinCount = winners.filter((item) => item == 'Player').length;
+    const cWinCount = winners.filter((item) => item == 'Computer').length;
+    const draws = winners.filter((item) => item == 'Tie').length;
+}
 
 // const buttons = document.querySelectorAll('button');
 
@@ -124,5 +139,6 @@ function checkRoundWin(choiceP, choiceC) {
 // function logWins() {
 //     console.log(winners);
 // }
+startGame();
 
-game();
+// game();
